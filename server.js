b1287461bbebcb3a49567cd19b8d33aec3ed3bf2
Joseph Adogeri <jadogeri@uno.edu>
2023-10-31 01:02:31 -0500

    added  cors to package.json

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const dotenv = require('dotenv').config();
 const errorHandler = require('./middleware/errorHandler');
 const connectDB = require('./config/dbConnection');
 
-
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 connectDB();
 const app = express();
 
 app.use(express.json())
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 
 app.use('/api/contacts', require('./routes/contactRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
